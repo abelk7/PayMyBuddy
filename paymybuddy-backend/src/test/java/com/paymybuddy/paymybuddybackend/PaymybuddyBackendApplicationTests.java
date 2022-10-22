4edc2d4534d439085b9898d82780972d972da0ff
Abel.K <kisebela.a@gmail.com>
2022-10-22 12:02:29 +0200

    PAY-1 : Initilise backend projet

---
+++ paymybuddy-backend/src/test/java/com/paymybuddy/paymybuddybackend/PaymybuddyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymybuddy.paymybuddybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
